39c86bff73dd61b229d966330cd5a9d55cf7abd2
Unknown <ajswago@gmail.com>
2019-05-24 14:02:09 -0600

    Initial commit from Spring Initializr

---
+++ src/main/java/com/swago/eightballapi/EightballapiApplication.java
@@ -0,0 +1,13 @@
+package com.swago.eightballapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightballapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EightballapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swago/eightballapi/EightballapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swago.eightballapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EightballapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
